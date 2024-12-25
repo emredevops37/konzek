d77c1271d1f3827134b1d537a9dece81a502a37f
Emre_Aktas <eaktas@akimmetal.com.tr>
2024-12-25 15:33:04 +0300

    a

---
+++ dockerlar/app.py
@@ -0,0 +1,18 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import socket
+
+host = "0.0.0.0"
+port = 8080
+
+class CustomHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        hostname = socket.gethostname()  # Container'ın hostname'ini al
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(f"Hello, World from {hostname}!".encode())  # Yanıt mesajına hostname ekle
+
+if __name__ == "__main__":
+    with HTTPServer((host, port), CustomHandler) as server:
+        print(f"Server running on {host}:{port}")
+        server.serve_forever()
